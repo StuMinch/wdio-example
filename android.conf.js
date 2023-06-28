d56759e7f6200a9e37864d9ff6dcabae7f9e6e45
Stuart Minchington <stumin@saucelabs.com>
2023-06-28 14:53:13 -0700

    Testing Microsoft Edge on Windows 11

---
+++ android.conf.js
@@ -0,0 +1,26 @@
+exports.config = {
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    logLevel: 'debug',
+    logOutput: '/Users/stuart/Developer/WebdriverIO/wdio-example/webdriver.log',
+    services: ['sauce'],
+    maxInstances: 100,
+    maxInstancesPerCapability: 100,
+    capabilities: [{
+      browserName: 'chrome',
+      platformName: 'Android',
+      'appium:deviceName': 'Android GoogleAPI Emulator',
+      'appium:platformVersion': '12.0',
+      'appium:automationName': 'UIAutomator2',
+      'sauce:options': {
+        build: 'WDIO Cronjob - June 23rd',
+        name: 'Android Test',
+        appiumVersion: '2.0.0-beta66'
+      },
+    },
+    ],
+    specs: [
+      './test/**/*.js'
+    ]
+  };
+  
