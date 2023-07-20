7da27b6369e5e34263ffbbd94056c6c7a2d35ef8
Stuart Minchington <stumin@saucelabs.com>
2023-07-20 15:19:31 -0700

    Can successfully add an item to the cart

--- test/getContexts.test.1.js
+++ test/getContexts.test.1.js
@@ -10,6 +10,9 @@ describe('Guest Checkout Screen', () => {
   });
 
   it('should allow location', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const allowBtn = await $("//android.widget.TextView[@text='Allow']");
     await allowBtn.click();
 
@@ -18,6 +21,9 @@ describe('Guest Checkout Screen', () => {
   });
 
   it('should enter a zip code', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const zipCodeField = await $("//android.widget.TextView[@text='Enter City or Postal Code']");
     await zipCodeField.click();
     await zipCodeField.click();
@@ -30,21 +36,33 @@ describe('Guest Checkout Screen', () => {
   });
 
   it('should tap on store result', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const storeResult = await $("//android.widget.TextView[contains(@text,'#')]");
     await storeResult.click();
   });
 
   it('should dismiss the biometrics prompt', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const dismissBtn = await $("//android.widget.TextView[@text='No, Thanks']");
     await dismissBtn.click();
   });
 
   it('should tap the OK button', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const okBtn = await $("//android.widget.Button[@text='OK']");
     await okBtn.click();
   });
 
   it('should search for product', async () => {
+    let contexts = await driver.getContexts();
+    console.log(contexts);
+
     const tapScreen = await $("android.widget.RelativeLayout");
     await tapScreen.click();
     const searchField = await $("//android.widget.EditText[@text='What can we help you find?']");
@@ -58,17 +76,46 @@ describe('Guest Checkout Screen', () => {
     });
 
     it('should tap on search result', async () => {
+      let contexts = await driver.getContexts();
+      console.log(contexts);
+
       const searchResult = await $("//android.widget.TextView[@text='Whirlpool']");
-      await searchResult.click();
-      await searchResult.click();
+      await searchResult.waitForDisplayed({ timeout: 10000 });
       await searchResult.click();
     });
 
     it('should add item to cart', async () => {
-      await driver.execute('mobile: scroll', {'direction': 'down'});
-      const enterZipCode = await $("//android.widget.EditText[@text='A1A 1A1']");
+      let contexts = await driver.getContexts();
+      console.log(contexts);
+
+      const locateZipCode = `new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("A1A 1A1"))`;
+      const enterZipCode = $(`android=${locateZipCode}`);
       await enterZipCode.setValue('M1R 4E6');
-    })
+      
+      const locateAddToCart = `new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("Add to Cart"))`;
+      const addToCart = $(`android=${locateAddToCart}`);
+      await addToCart.click();
+    });
+
+    it('should wait for item to be added to the cart', async () => {
+      let contexts = await driver.getContexts();
+      console.log(contexts);
+
+      const addToCart = await $("//android.widget.Button[@text='Add to Cart']");
+      await addToCart.waitForDisplayed({ timeout: 10000 });
+    });
+
+    it('should view the cart', async () => {
+      const viewCartBtn = $("//*[@content-desc='View Cart']");
+      await viewCartBtn.waitForDisplayed({ timeout: 10000 });
+      await viewCartBtn.click();
+    });
+
+    it('should tap checkout now', async () => {
+      const checkoutNowBtn = $("//*[@id='btnCheckoutNow']");
+      await checkoutNowBtn.waitForDisplayed({ timeout: 5000 });
+      await checkoutNowBtn.click();
+    });
 
 });
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   framework: 'mocha',
     mochaOpts: {
-        timeout: 20000
+        timeout: 40000
     },
   user: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
@@ -17,11 +17,11 @@ exports.config = {
     //'appium:deviceName': 'Google Pixel 6 GoogleAPI Emulator',
     'appium:platformVersion': '12.0',
     'appium:deviceOrientation': 'portrait',
-    'appium:app': 'storage:filename=app-qa-firebase.apk',
+    'appium:app': 'storage:filename=home-depot-app.apk',
     'appium:automationName': 'UIAutomator2',
     'sauce:options': {
       build: 'Guest Checkout WebView Troubleshooting'
-    //  appiumVersion: '2.0.0-beta66'
+      //appiumVersion: '2.0.0-beta66'
     },
   },
   ],
